1ddeaa29602ffb95ed94c7a1c4b75a00c49e853f
emmaanuel <emmanuel@iMac-de-Emmanuel.local>
2016-03-17 00:37:09 +0100

    init

---
+++ arroseur.ino
@@ -0,0 +1,36 @@
+
+#define sensorPinD 3
+#define vccPin 1
+#define moteurPin 2
+#define ledPin 4
+
+int digitalValue = 0;
+
+ 
+void setup() {
+   //pinMode(sensorPinD, INPUT); 
+   pinMode(vccPin, OUTPUT); 
+   pinMode(moteurPin, OUTPUT); 
+   pinMode(ledPin, OUTPUT);
+   digitalWrite(vccPin, LOW);
+   digitalWrite(moteurPin, LOW);
+   digitalWrite(ledPin, LOW);
+}
+ 
+void loop() {
+ digitalWrite(vccPin, HIGH);
+  delay(1000);  
+  digitalValue = analogRead(sensorPinD);    
+  digitalWrite(vccPin, LOW);
+  if (digitalValue>650){
+    digitalWrite(ledPin, HIGH);
+    digitalWrite(moteurPin, HIGH);
+    delay(digitalValue*10);
+    digitalWrite(ledPin, LOW);
+    digitalWrite(moteurPin, LOW);
+  }
+ delay(10000); 
+
+
+      
+}
